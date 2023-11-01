0dc88f2d06c4be02f49c743a308845c116293cdc
nbarnum <nbarnum@users.noreply.github.com>
2023-11-01 23:25:04 +0000

    switch type from Image to Zip

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -10,6 +10,12 @@ variable "bootstrap_image_tag" {
   description = "The container image tag for the initial bootstrap image. This value will be ignored after initial apply."
 }
 
+variable "handler" {
+  default     = "index.handler"
+  type        = string
+  description = "Function entrypoint in your code."
+}
+
 variable "environment_variables" {
   type = map(string)
   default = {
@@ -28,4 +34,20 @@ variable "memory_size" {
   default     = 128
   type        = number
   description = "Amount of memory in MB your Lambda Function can use at runtime. Defaults to 128. Ref: https://docs.aws.amazon.com/lambda/latest/dg/limits.html"
+}
+
+variable "runtime" {
+  default     = "nodejs18.x"
+  type        = string
+  description = "Identifier of the function's runtime. See https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime for valid values."
+}
+
+variable "package_type" {
+  default     = "Zip"
+  type        = string
+  description = "Lambda deployment package type. Defaults to Zip."
+  validation {
+    condition     = contains(["Image", "Zip"], var.package_type)
+    error_message = "Valid values are Image and Zip."
+  }
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -16,14 +16,25 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+data "archive_file" "bootstrap_lambda" {
+  type        = "zip"
+  output_path = "${path.module}/lambda_function_bootstrap.zip"
+
+  source {
+    content  = "hello"
+    filename = "bootstrap.txt"
+  }
+}
+
 resource "aws_lambda_function" "test_lambda" {
   function_name = var.function_name
   role          = aws_iam_role.iam_for_lambda.arn
 
-  # Initial bootstrap container image intended to be overwritten by deploy pipelines in external CI/CD systems.
-  # NOTE: The bootstrap image must already exist in ECR before first apply.
-  image_uri    = "${var.bootstrap_image_name}:${var.bootstrap_image_tag}"
-  package_type = "Image"
+  filename         = data.archive_file.bootstrap_lambda.output_path
+  package_type     = var.package_type
+  handler          = var.handler
+  runtime          = var.runtime
+  source_code_hash = data.archive_file.bootstrap_lambda.output_base64sha256
 
   memory_size = var.memory_size
 
@@ -33,8 +44,6 @@ resource "aws_lambda_function" "test_lambda" {
 
   # Ignore changes to the image URI made by external CI/CD
   lifecycle {
-    ignore_changes = [
-      image_uri
-    ]
+    ignore_changes = [source_code_hash]
   }
 }
